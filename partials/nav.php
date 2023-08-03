dc14d2d840a05564af3f7242323073ad79e27c32
mohammad-kiyam <mk42@njit.edu>
2023-08-03 02:17:20 -0400

    added watchlist and data mapping

--- partials/nav.php
+++ partials/nav.php
@@ -33,6 +33,7 @@ session_start();
             <li><a href="<?php echo get_url('home.php'); ?>">Home</a></li>
             <li><a href="<?php echo get_url('profile.php'); ?>">Profile</a></li>
             <li><a href="<?php echo get_url('dashboard.php'); ?>">Dashboard</a></li>
+            <li><a href="<?php echo get_url('watchlist.php'); ?>">Watchlist</a></li>
         <?php endif; ?>
         <?php if (!is_logged_in()) : ?>
             <li><a href="<?php echo get_url('login.php'); ?>">Login</a></li>
---
+++ public_html/Project/watchlist.php
@@ -0,0 +1,38 @@
+<?php
+
+require(__DIR__ . "/../../partials/nav.php");
+?>
+<h1>Watchlist</h1>
+
+<?php
+
+if (is_logged_in(true)) {
+    //comment this out if you don't want to see the session variables
+    error_log("Session data: " . var_export($_SESSION, true));
+}
+
+$user_id = get_user_id();  // Assuming you have a function get_user_id() to retrieve the current logged in user ID
+$db = getDB();  // Assuming getDB() is a function that returns a PDO instance
+
+$stmt = $db->prepare("SELECT movie_title, image_url FROM Watchlist WHERE user_id = :user_id");
+$stmt->execute([":user_id" => $user_id]);
+
+$movies = $stmt->fetchAll(PDO::FETCH_ASSOC);
+
+if ($movies) {
+    echo '<ul>';
+    foreach ($movies as $movie) {
+        echo '<li>';
+        echo 'Title: ' . htmlspecialchars($movie["movie_title"]), "<br>";
+        if ($movie["image_url"]) {
+            echo '<img src="' . htmlspecialchars($movie["image_url"]) . '" alt="Movie Poster" style="max-width: 200px; max-height: 300px;">', "<br><br><br>";
+        }
+        echo '</li>';
+    }
+    echo '</ul>';
+} else {
+    echo 'No movies in your watchlist.';
+}
+
+require(__DIR__ . "/../../partials/flash.php");
+?>
