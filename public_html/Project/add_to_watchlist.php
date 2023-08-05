80b4c1b5c7d605c7be64caea2a1710ae7b4e02dd
mohammad-kiyam <mk42@njit.edu>
2023-08-04 23:14:35 -0400

    duplicate handling

--- public_html/Project/add_to_watchlist.php
+++ public_html/Project/add_to_watchlist.php
@@ -7,30 +7,38 @@ if (!is_logged_in()) {
 }
 
 if (isset($_POST["movies"]) && isset($_POST["images"])) {
+    $user_id = get_user_id();
     $movies = $_POST["movies"];
     $images = $_POST["images"];
     
-
-    $user_id = get_user_id(); 
-
     $db = getDB();
 
     for ($i = 0; $i < count($movies); $i++) {
-        $stmt = $db->prepare("INSERT INTO Watchlist (user_id, movie_title, image_url) VALUES (:user_id, :movie_title, :image_url)");
+        // Check if the movie already exists in the user's watchlist
+        $stmt = $db->prepare("SELECT 1 FROM Watchlist WHERE user_id = :user_id AND movie_title = :movie_title");
+        $stmt->execute([":user_id" => $user_id, ":movie_title" => $movies[$i]]);
+        $exists = $stmt->fetchColumn();
 
-        $r = $stmt->execute([
-            ":user_id" => $user_id,
-            ":movie_title" => $movies[$i],
-            ":image_url" => $images[$i]
-        ]);
+        if ($exists) {
+            flash("'$movies[$i]' is already in your watchlist.", 'warning');
+        } else {
+            // If movie not in watchlist, then insert it
+            $stmt = $db->prepare("INSERT INTO Watchlist (user_id, movie_title, image_url) VALUES (:user_id, :movie_title, :image_url)");
+            $r = $stmt->execute([
+                ":user_id" => $user_id,
+                ":movie_title" => $movies[$i],
+                ":image_url" => $images[$i]
+            ]);
 
-        if (!$r) {
-            flash("There was a problem saving your movie. Please try again.");
+            if (!$r) {
+                flash("There was a problem saving '$movies[$i]'. Please try again.", 'danger');
+            } else {
+                flash("'$movies[$i]' added to watchlist.", 'success');
+            }
         }
     }
 
-    flash("Movies successfully added to watchlist", "success");
-    die(header("Location: watchlist.php"));
+    // Redirect back to dashboard after processing, may remove this later on
+    die(header("Location: dashboard.php"));
 }
 ?>
-
