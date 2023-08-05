172e0a26a1ca65fe45ee9839a506013944c8285d
mohammad-kiyam <mk42@njit.edu>
2023-08-04 21:24:18 -0400

    filter and sort features

--- public_html/Project/watchlist.php
+++ public_html/Project/watchlist.php
@@ -1,7 +1,49 @@
 <?php
 require(__DIR__ . "/../../partials/nav.php");
+
+$user_id = get_user_id();
+$db = getDB();
+
+if (isset($_POST["remove_movies"])) {
+    $movies_to_remove = $_POST["remove_movies"];
+    flash('Movie Successfully Removed', "success");
+
+    $inQuery = implode(',', array_fill(0, count($movies_to_remove), '?'));
+    array_unshift($movies_to_remove, $user_id);
+    $stmt = $db->prepare("DELETE FROM Watchlist WHERE user_id = ? AND movie_title IN ($inQuery)");
+
+    $stmt->execute($movies_to_remove);
+}
+
+$order = "DESC"; // default order
+if (isset($_POST['filter'])) {
+    if ($_POST['filter'] == "oldest") {
+        $order = "ASC"; 
+    }
+}
+
+$limit = 10; // default limit
+if (isset($_POST['limit']) && is_numeric($_POST['limit']) && $_POST['limit'] > 0) {
+    $limit = (int)$_POST['limit']; 
+}
+
+$query = "SELECT movie_title, image_url FROM Watchlist WHERE user_id = :user_id ORDER BY created $order LIMIT $limit";
+$stmt = $db->prepare($query);
+$stmt->execute([":user_id" => $user_id]);
+$movies = $stmt->fetchAll(PDO::FETCH_ASSOC);
+
 ?>
 
+<form method="POST" id="filterForm">
+    <label for="filter">Sort:</label>
+    <select id="filter" name="filter">
+        <option value="newest" <?php echo $order=="DESC"?'selected':''; ?>>Newest First</option>
+        <option value="oldest" <?php echo $order=="ASC"?'selected':''; ?>>Oldest First</option>
+    </select>
+    <label for="limit">Show:</label>
+    <input id="limit" name="limit" type="number" value="<?php echo $limit; ?>">
+    <input type="submit" value="Apply Filter">
+</form>
 
 <?php
 
@@ -9,26 +51,6 @@ if (is_logged_in(true)) {
     //comment this out if you don't want to see the session variables
     error_log("Session data: " . var_export($_SESSION, true));
 }
-
-$user_id = get_user_id(); 
-$db = getDB();  
-
-if (isset($_POST["remove_movies"])) {
-    $movies_to_remove = $_POST["remove_movies"];
-    flash('Movie Successfully Removed', "success");
-    
-    $inQuery = implode(',', array_fill(0, count($movies_to_remove), '?'));
-    array_unshift($movies_to_remove, $user_id);
-    $stmt = $db->prepare("DELETE FROM Watchlist WHERE user_id = ? AND movie_title IN ($inQuery)");
-    
-    $stmt->execute($movies_to_remove);
-}
-
-
-$stmt = $db->prepare("SELECT movie_title, image_url FROM Watchlist WHERE user_id = ?");
-$stmt->execute([$user_id]);
-
-$movies = $stmt->fetchAll(PDO::FETCH_ASSOC);
 
 if ($movies) {
     echo '<h1 class="watchlist-title">My Watchlist</h1>';
@@ -51,12 +73,9 @@ if ($movies) {
 } else {
     echo 'No movies in your watchlist.';
 }
-?>
 
-<?php
 require(__DIR__ . "/../../partials/flash.php");
 ?>
-
 
 <script>
     function checkForm() {
@@ -68,4 +87,12 @@ require(__DIR__ . "/../../partials/flash.php");
         }
         return true;
     }
+
+    document.getElementById('filterForm').addEventListener('submit', function(event) {
+    var limitInput = document.getElementById('limit');
+    if (limitInput.value < 1) {
+        event.preventDefault();
+        flash('Please enter a positive number that is greater than 0', "danger");
+    }
+});
 </script>
