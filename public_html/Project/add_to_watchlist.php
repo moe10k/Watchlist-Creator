91fdca9719a0d9e393be03c53273a47c7844dc76
mohammad-kiyam <mk42@njit.edu>
2023-08-08 00:09:09 -0400

    MS3 watchlist

--- public_html/Project/add_to_watchlist.php
+++ public_html/Project/add_to_watchlist.php
@@ -6,10 +6,14 @@ if (!is_logged_in()) {
     die(header("Location: login.php"));
 }
 
-if (isset($_POST["movies"]) && isset($_POST["images"])) {
-    $user_id = get_user_id();
+if (isset($_POST["movies"]) && isset($_POST["images"]) && isset($_POST["yearrelease"]) && isset($_POST["yearplayed"]) && isset($_POST["type"]) && isset($_POST["famousactors"])) { //this creates variables out of the data saved
+    $user_id = get_user_id();                                                                                                                                                      //in dashboard.php
     $movies = $_POST["movies"];
     $images = $_POST["images"];
+    $yearrelease = $_POST["yearrelease"];
+    $yearplayed = $_POST["yearplayed"];
+    $type = $_POST["type"];
+    $famousactors = $_POST["famousactors"];
     
     $db = getDB();
 
@@ -21,11 +25,15 @@ if (isset($_POST["movies"]) && isset($_POST["images"])) {
         if ($exists) {
             flash("'$movies[$i]' is already in your watchlist.", 'warning');
         } else {
-            $stmt = $db->prepare("INSERT INTO Watchlist (user_id, movie_title, image_url) VALUES (:user_id, :movie_title, :image_url)");
+            $stmt = $db->prepare("INSERT INTO Watchlist (user_id, movie_title, image_url, year_release, year_played, type, famous_actors) VALUES (:user_id, :movie_title, :image_url, :year_release, :year_played, :type, :famous_actors)");
             $r = $stmt->execute([
                 ":user_id" => $user_id,
                 ":movie_title" => $movies[$i],
-                ":image_url" => $images[$i]
+                ":image_url" => $images[$i],
+                ":year_release" => $yearrelease[$i],
+                ":year_played" => $yearplayed[$i],
+                ":type" => $type[$i],
+                ":famous_actors" => $famousactors[$i]
             ]);
 
             if (!$r) {
